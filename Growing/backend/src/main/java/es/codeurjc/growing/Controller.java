53597bee440f485ea11671a0f83dde67cbc168a8
jorge-ap <j.ad.p2370@gmail.com>
2021-03-02 10:44:30 +0100

    Creating backend project

---
+++ Growing/backend/src/main/java/es/codeurjc/growing/Controller.java
@@ -0,0 +1,15 @@
+package es.codeurjc.growing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/index")
+    public String greeting(Model model) {
+        return "index";
+    }
+
+}

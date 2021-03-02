a8cb3eca2d053cbe0d2d85327cdd720a106ae5b6
jorge-ap <j.ad.p2370@gmail.com>
2021-03-03 00:23:31 +0100

    Creation of project and moustache preparation
    - In this commit, I fixed the problems I had creating the project, generating a lot of file changes.
    - Besides, I added the controller navigation and some moustache code in categoryInfo.html

--- Growing/backend/src/main/java/es/dawequipo3/growing/GrowingController.java
+++ Growing/backend/src/main/java/es/dawequipo3/growing/GrowingController.java
@@ -2,6 +2,7 @@ package es.dawequipo3.growing;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 
 @Controller
@@ -10,5 +11,49 @@ public class GrowingController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/categories")
+    public String categories(){
+        return "categories";
+    }
+
+    @GetMapping("/explore")
+    public String explore(){
+        return "explore";
+    }
+
+    @GetMapping("/aboutUs")
+    public String aboutUs(){
+        return "AboutUs";
+    }
+
+    @GetMapping("/getStarted")
+    public String getStarted(){
+        return "getStarted";
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "profile";
+    }
+
+    @GetMapping("/profileAdmin")
+    public String profileAdmin(){
+        return "profileAdmin";
+    }
+
+    @GetMapping("/categoryInfo")
+    public String categoryInfo(Model model){
+
+        model.addAttribute("title", "Mental Health");
+        model.addAttribute("description", "In this category we show you a group of plans that you must carry out every day to\n" +
+                "                    improve your mental health. The tree will increase its height when you complete some plans and it\n" +
+                "                    will lower when you don't.");
+        model.addAttribute("date", "11/03/2020");
+
+        model.addAttribute("registered", false);
+        model.addAttribute("liked", true);
+        return "categoryInfo";
     }
 }

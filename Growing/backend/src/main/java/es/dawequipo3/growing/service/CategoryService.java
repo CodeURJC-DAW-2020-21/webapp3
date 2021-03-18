fd5874fa800f1d1da5548ece9edea546221bfce8
jorge-ap <j.ad.p2370@gmail.com>
2021-03-18 20:49:59 +0100

    Uploading missing files
    In the last commit, this files weren't uploaded and don't know why, so this commit has as only purpose upload those files

--- Growing/backend/src/main/java/es/dawequipo3/growing/service/CategoryService.java
+++ Growing/backend/src/main/java/es/dawequipo3/growing/service/CategoryService.java
@@ -28,33 +28,6 @@ public class CategoryService {
     @Autowired
     private TreeService treeService;
 
-    @PostConstruct
-    public void init(){
-
-        Category mentalHealth = new Category("Mental health", "Because we know that having a good mental health is fundamental, we want to share with you some\n" +
-                "                        tasks for helping you to achieve it", "ri-mental-health-line icon", "blue");
-        this.save(mentalHealth);
-
-        Category physicalHealth = new Category("Physical health", "Do you want to be fit making regular exercise but you are a lazy person? Look at the plans of\n" +
-                "                        this category and reject the lazyness!", "ri-run-line icon", "red");
-        this.save(physicalHealth);
-
-        Category savings = new Category("Savings", "Everyone want to have money but most of the people waste it in stuff they don't need. In this\n" +
-                "                        category we will help you with some tips\n" +
-                "                        to increase your savings and not wasting them.", "ri-money-euro-circle-line icon", "green");
-        this.save(savings);
-
-        Category goodNight = new Category("Good night", "Do you have insomnia? Here you will see multiple tasks that you can do to sleep like a baby\n" +
-                "                        during the night", "ri-hotel-bed-fill icon", "bluenavy");
-        this.save(goodNight);
-
-        Category home = new Category("Home", "Do you have to do home tasks but you leave them to the end or you even make them? This is your\n" +
-                "                        category! You won't forget them and finish them quickly", "ri-home-2-line icon", "orange");
-        this.save(home);
-
-        this.save(new Category("Focus and Effort", "Do you want to be challenged daily? I challenge you to follow the pace of this category and\n" +
-                "                        complete the refreshing daily challenge", "ri-focus-2-line icon", "purple"));
-    }
 
     public void save(Category category){
         categoryRepository.save(category);

a4118151ea6388c7db62eb34dadd77e7a5a52896
Mrcsrobles <mrcsrobles@gmail.com>
2021-04-08 21:50:33 +0200

    Complete the RESTCategory

--- backend/src/main/java/es/dawequipo3/growing/controllerREST/RESTCategory.java
+++ backend/src/main/java/es/dawequipo3/growing/controllerREST/RESTCategory.java
@@ -26,6 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collection;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 
@@ -46,7 +47,7 @@ public class RESTCategory {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "Returns all the existing categories",
+                    description = "Categroies retrieved correctly",
                     content = {@Content(
                             schema = @Schema(implementation = CategoryDetails.class)
                     )}
@@ -64,14 +65,14 @@ public class RESTCategory {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "Returns the category information",
+                    description = "Category's information retrieved correctly",
                     content = {@Content(
                             schema = @Schema(implementation = CategoryDetails.class)
                     )}
             ),
             @ApiResponse(
                     responseCode = "404",
-                    description = "Doesn't exists a category with that name",
+                    description = "Category not found",
                     content = @Content
             ),
     })
@@ -97,14 +98,14 @@ public class RESTCategory {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "Returns the created category information",
+                    description = "Category created correctly",
                     content = {@Content(
                             schema = @Schema(implementation = CategoryDetails.class)
                     )}
             ),
             @ApiResponse(
                     responseCode = "403",
-                    description = "Only access to admin account",
+                    description = "Permission error, only access to admin account",
                     content = @Content
             ),
             @ApiResponse(
@@ -140,53 +141,97 @@ public class RESTCategory {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "Returns the created category information",
+                    description = "Category edited correctly",
                     content = {@Content(
                             schema = @Schema(implementation = CategoryDetails.class)
                     )}
             ),
             @ApiResponse(
                     responseCode = "403",
-                    description = "Only access to admin account",
+                    description = "Permission error, only access to admin account",
                     content = @Content
-            ),
-            @ApiResponse(
-                    responseCode = "409",
-                    description = "There is already a category with the name given by parameter",
-                    content = @Content
-            ),
+            )
     })
     @JsonView(CategoryDetails.class)
     @PutMapping("/edit")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Category> editCategory(@RequestParam String categoryName, @RequestParam String newDescription,
-                                                   @RequestParam String color, MultipartFile imageFile) throws IOException {
+    public ResponseEntity<Category> editCategory(@RequestParam String categoryName, @RequestParam(required = false) String newDescription,
+                                                   @RequestParam(required = false) String color, MultipartFile imageFile) throws IOException {
 
         Optional<Category> op = categoryService.findByName(categoryName);
         if (op.isPresent()){
             Category category = op.get();
+            if (newDescription==null){
+                newDescription="";
+            }
+            if (color==null){
+                color="";
+            }
             categoryService.editCategory(category, newDescription, color, imageFile);
             return ResponseEntity.ok(category);
         }
          else return ResponseEntity.notFound().build();
     }
+    @Operation(summary = "Remove the like of a category as the logged user")
 
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Like removed correctly",
+                    content = {@Content(
+                            schema = @Schema(implementation = CategoryDetails.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Permission error, only access to logged users",
+                    content = @Content
+            ),
+            @ApiResponse(
+            responseCode = "404",
+            description = "Category not found",
+            content = @Content
+    )
+    })
     @JsonView(CategoryDetails.class)
     @PutMapping("/dislike")
-    public ResponseEntity<Category> dislikeCategory(@RequestParam String categoryName, HttpServletRequest request){
+    public ResponseEntity<Category> dislikeCategory(@RequestParam String categoryName, HttpServletRequest request) {
         String email = request.getUserPrincipal().getName();
-        User user = userService.findUserByEmail(email).orElseThrow();
-        Optional<Category> op = categoryService.findByName(categoryName);
-        if (op.isPresent()){
-            Category category = op.get();
-            user.getUserFavoritesCategory().remove(category);
-            category.setLikedByUser(false);
-            userService.update(user);
-            return ResponseEntity.ok(category);
+        try {
+            User user = userService.findUserByEmail(email).orElseThrow();
+            Optional<Category> op = categoryService.findByName(categoryName);
+            if (op.isPresent()) {
+                Category category = op.get();
+                user.getUserFavoritesCategory().remove(category);
+                category.setLikedByUser(false);
+                userService.update(user);
+                return ResponseEntity.ok(category);
+            } else return ResponseEntity.notFound().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-         else return ResponseEntity.notFound().build();
     }
+    @Operation(summary = "Like a category as the logged user")
 
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Like removed correctly",
+                    content = {@Content(
+                            schema = @Schema(implementation = CategoryDetails.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Permission error, only access to logged users",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Category not found",
+                    content = @Content
+            )
+    })
     @JsonView(CategoryDetails.class)
     @PutMapping("/like")
     public ResponseEntity<Category> likeCategory(@RequestParam String categoryName, HttpServletRequest request){

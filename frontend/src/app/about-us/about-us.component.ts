2318350cf627f906a43d05960f2de2a7b5a74cab
jorge-ap <j.adame.2018@alumnos.urjc.es>
2021-04-22 18:56:39 +0200

    Creating the about us screen component

---
+++ frontend/src/app/about-us/about-us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['../../assets/css/style.css']
+})
+export class AboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

16f0ca111df1c969739c7b66858e2baf8ca4ac60
jorge-ap <j.adame.2018@alumnos.urjc.es>
2021-04-28 13:54:35 +0200

    Create image extension for Angular
    In this commit, I expanded the create_image.sh to incorporate Angular build on it and dockerize.
    
    The other files commited correspond to a missing / in img src, which made a problem to show them

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-# First, we locate the pom
+# First, check the destination directory exists
+if [ -d "../backend/src/main/resources/static/new" ]
+then
+# Delete all the inside content 
+	rm -f ../backend/src/main/resources/static/new/*
+else
+# Create the folder
+	mkdir ../backend/src/main/resources/static/new
+fi
+
+# Go to frontend which contains the Angular project
+cd ../frontend
+
+# Build the application 
+ng build --prod --base-href="/new/"
+
+# Copy all the dist folder content into backend static folder
+cp dist/frontend/* ../backend/src/main/resources/static/new
+
+# Then locate the pom.xml
 cd ../backend || exit
 # Executing dockerfile commands
 sudo docker run --rm -v "$PWD":/data -w /data maven mvn package
@@ -10,6 +29,8 @@ cp target/*.jar ../docker
 
 # Return to docker folder
 cd ../docker || exit
+
+rm -f growing-0.0.1-SNAPSHOT.jar
 
 # Building the new container
 sudo docker build -t dawequipo3/growing .

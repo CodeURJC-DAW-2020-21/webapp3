5d549b6a2a5196f6cee7490479a81be6e74726ae
flavia29 <61882314+flavia29@users.noreply.github.com>
2021-04-30 20:34:51 +0200

    Added PDF converter to profile page

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { Title } from '@angular/platform-browser';
 import {ImageService} from "../service/image.service";
 import {BarChartComponent} from "../charts/barChart/barChart.component";
 import {RadarChartComponent} from "../charts/radar-chart/radar-chart.component";
+import * as html2pdf from 'html2pdf.js'
 
 
 @Component({
@@ -94,6 +95,22 @@ export class ProfileComponent implements OnInit {
     this.barChart.generateChart();
     this.radarChart.generateRadar();
 
+  }
+
+  downloadPDF(){
+    var day = new Date();
+    const options = {
+      filename: 'progress' + day.getMonth() + '-' + day.getDate() + '-' + day.getFullYear() + '.pdf',
+      image: {type: 'jpeg', quality: 10.98},
+      html2canvas: {scale: 3, letterRendering: true,},
+      jsPDF:{orientation: 'portrait', unit: "in", format: "a4",}
+    }
+    const progress: Element = document.getElementById('chartsPDF');
+    html2pdf()
+      .from(progress)
+      .set(options)
+      .save()
+      .catch(err => console.log(err));
   }
 
 }

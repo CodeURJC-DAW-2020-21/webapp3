8399e4b0adfdc5b75dd28ea6a20a9736bde13e5f
jorge-ap <j.ad.p2370@gmail.com>
2021-05-02 19:33:51 +0200

    commit to merge

--- frontend/src/app/service/category.service.ts
+++ frontend/src/app/service/category.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../model/Category';
-import { map } from 'rxjs/operators';
+
 
 
 @Injectable({
@@ -18,25 +18,6 @@ export class CategoryService {
     return this.httpClient.get(this.urlPrefix + '/') as Observable<Category[]>
   }
 
-  getCategoryLabels(): Observable<string[]> {
-    return this.httpClient.get(this.urlPrefix + '/').pipe(
-      map(response => this.getName(response as any))
-    )
-  }
-
-  private getName(response) {
-    return response.map(category => category.name);
-  }
-
-  getCategoryColors(): Observable<string[]> {
-    return this.httpClient.get(this.urlPrefix + '/').pipe(
-      map(response => this.getColor(response as any))
-    )
-  }
-
-  private getColor(response) {
-    return response.map(category => category.color);
-  }
 
   public getCategoryIcon(categoryName: string):string{
     return 'https://localhost:8443'+this.urlPrefix+'/image?categoryName='+categoryName;
